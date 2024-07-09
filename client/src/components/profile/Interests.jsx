5918b6e18b138991e83b1ec5630f8146608e02f5
TiTY <tee25450@gmail.com>
2024-07-09 15:00:18 +0700

    feat:add and style step2 of register page

---
+++ client/src/components/profile/Interests.jsx
@@ -0,0 +1,189 @@
+import { useState } from "react";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+function IdentitiesAndInterests() {
+  //states form 2
+
+  const [sexualIdentity, setSexualIdentity] = useState("");
+  const [sexualPreference, setSexualPreference] = useState("");
+  const [racialPreference, setRacialPreference] = useState("");
+  const [meetingInterest, setMeetingInterest] = useState("");
+
+  //states hobbies
+  const [hobby, setHobby] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const newHobbies = [...hobby, inputValue];
+      setHobby(newHobbies);
+      setInputValue("");
+    }
+  };
+  const deleteHobby = (index) => {
+    const newHobbies = [...hobby];
+    newHobbies.splice(index, 1);
+    setHobby(newHobbies);
+  };
+  return (
+    <div className="w-[100%] h-[80%] flex justify-center mb-[223px]">
+      <div className="flex flex-col  w-[full]">
+        <h1 className="IdentitiesAndInterestes text-[#A62D82] mt-[20px] font-[700] text-[24px] leading-[30px]">
+          Identities and Interestes
+        </h1>
+        <div className="column1 flex">
+          <div className="flex flex-col mr-[12px] mt-[24px]">
+            <label
+              htmlFor="SexualIdentities"
+              className="font-[400] text-[16px] leading-[24px]"
+            >
+              Sexual identities
+            </label>
+            <select
+              className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg p-2"
+              id="name"
+              value={sexualIdentity}
+              onChange={(event) => {
+                setSexualIdentity(event.target.value);
+              }}
+            >
+              <option disabled value="">
+                Select Your Sexual Identity
+              </option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col ml-[12px] mt-[24px]">
+            <label
+              htmlFor="SexualPreferences"
+              className="font-[400] text-[16px] leading-[24px]"
+            >
+              Sexual preferences
+            </label>
+
+            <select
+              className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg p-2"
+              id="name"
+              value={sexualPreference}
+              onChange={(event) => {
+                setSexualPreference(event.target.value);
+              }}
+            >
+              <option disabled value="">
+                Select Your Sexual Preference
+              </option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+        </div>
+        <div className="column2 flex">
+          <div className="flex flex-col mr-[12px] mt-[24px]">
+            <label
+              htmlFor="RacialPreferences"
+              className="font-[400] text-[16px] leading-[24px]]"
+            >
+              Racial preferences
+            </label>
+
+            <select
+              className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg p-2"
+              id="name"
+              value={racialPreference}
+              onChange={(event) => {
+                setRacialPreference(event.target.value);
+              }}
+            >
+              <option disabled value="">
+                Select Your Sexual Preference
+              </option>
+              <option value="Asian">Asian</option>
+              <option value="Black or African American">
+                Black or African American
+              </option>
+              <option value="Hispanic or Latino">Hispanic or Latino</option>
+              <option value="Native American or Alaska Native">
+                Native American or Alaska Native
+              </option>
+              <option value="Native Hawaiian or Other Pacific Islander">
+                Native Hawaiian or Other Pacific Islander
+              </option>
+              <option value="White">White</option>
+              <option value="Prefer not to say">Prefer not to say</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col ml-[12px] mt-[24px]">
+            <label
+              htmlFor="MeetingInterests"
+              className="font-[400] text-[16px] leading-[24px]"
+            >
+              Meeting interests
+            </label>
+
+            <select
+              className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg p-2"
+              id="name"
+              value={meetingInterest}
+              onChange={(event) => {
+                setMeetingInterest(event.target.value);
+              }}
+            >
+              <option disabled value="">
+                Select Your Meeting Interests
+              </option>
+              <option value="Long-term">Long-term Relationships</option>
+              <option value="Casual">Casual Dating</option>
+              <option value="Friendship">Friendship</option>
+              <option value="Niche">Niche Dating</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="column3 flex flex-col">
+          <div className="flex flex-col mr-[12px] mt-[24px]">
+            <label
+              htmlFor="Hobbies"
+              className="font-[400] text-[16px] leading-[24px]]"
+            >
+              Hobbies / Interests (Maximum 10)
+            </label>
+            <div className="flex flex-row border-[1px] border-[#D6D9E4] rounded-[8px] gap-[8px] pt-[12px] pr-[16px] pb-[12px] pl-[12px] w-[930px] h-[53px]">
+              {hobby.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex items-center px-[4px] py-[8px] gap-[8px] rounded-[6px] bg-[#F4EBF2] text-[#7D2262]"
+                >
+                  <span className="">{item}</span>
+                  <button className="" onClick={() => deleteHobby(index)}>
+                    x
+                  </button>
+                </div>
+              ))}
+              <input
+                className="border-[1px] font-normal border-none rounded-lg py-[12px] px-[12px] focus:outline-none w-full"
+                type="text"
+                id="Hobbies"
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IdentitiesAndInterests;
--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import IdentitiesAndInterests from "../../components/profile/Interests";
 import ProfilePictures from "../../components/profile/ProfilePictures";
 import BasicInformation from "../../components/profile/Information";
 
@@ -25,7 +25,7 @@ const RegisterPage = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="font-Nunito bg-[url('/src/assets/register/register-section-bg.svg')]  flex flex-col items-center justify-center w-screen h-screen bg-cover">
         <div className="flex justify-center items-center flex-col w-4/5 h-[950px]">
           <header className="flex flex-row items-center justify-evenly w-[90vw] h-[20%] mt-[150px] mb-[2%]">
             <div className="flex flex-col items-start justify-center">
@@ -122,55 +122,61 @@ const RegisterPage = () => {
           </header>
           {/* Form input */}
           {step === 1 && <BasicInformation />}
-          {step === 2 && (
-            <div className="w-full flex flex-col items-center">
-              {/* Add step 2 form fields here */}
-            </div>
-          )}
+          {step === 2 && <IdentitiesAndInterests />}
           {step === 3 && (
             <div className="w-full flex flex-col items-center">
               {<ProfilePictures />}
             </div>
           )}
           {/*  Button  */}
-          <div className="flex w-full justify-end mt-4">
-            {/* Back button */}
-            <button
-              onClick={handleBack}
-              type="button"
-              className="text-[#C70039] hover:text-black font-[800] "
-            >
-              🡐 Back
-            </button>
+          <div className=" w-[100vw] h-[10vh] mt-5 border-t border-[#E4E6ED] bg-[white] mb-5 pb-10 flex flex-col items-center justify-center">
+            <div className=" w-[100vw]">
+              <div className=" w-[100%] mt-[2%] flex flex-row justify-center content-end bg-[white]">
+                <p className="flex justify-start items-start content-start mt-[1.5%] mr-[70%]">
+                  {step === 1 && "1/3"} {step === 2 && "2/3"}{" "}
+                  {step === 3 && "3/3"}
+                </p>
 
-            {/* Next button */}
-            {step === 1 && (
-              <button
-                type=""
-                onClick={handleNext}
-                className="mt-[0.5%] text-white bg-[#C70039] hover:bg-red-800  font-[800] rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-5"
-              >
-                Next step
-              </button>
-            )}
-            {step === 2 && (
-              <button
-                type=""
-                onClick={handleNext}
-                className="mt-[0.5%] text-white  font-[800] bg-[#C70039] hover:bg-red-800 rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-5"
-              >
-                Next step
-              </button>
-            )}
+                {/* Go Back button */}
+                <button
+                  onClick={handleBack}
+                  type="button"
+                  className="text-[#C70039] hover:text-black font-[800] "
+                >
+                  🡐 Back
+                </button>
 
-            {step === 3 && (
-              <button
-                type="submit"
-                className="mt-[0.5%] text-white bg-[#C70039] hover:bg-red-800 font-[700] rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 disabled:bg-[#F1F2F6] disabled:text-[#646D89] ml-5"
-              >
-                Confirm
-              </button>
-            )}
+                {/* Go Next button */}
+                {step === 1 && (
+                  <button
+                    type=""
+                    onClick={handleNext}
+                    className="mt-[0.5%] text-white bg-[#C70039] hover:bg-red-800  font-[800] rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-5"
+                  >
+                    Next step
+                  </button>
+                )}
+
+                {step === 2 && (
+                  <button
+                    type=""
+                    onClick={handleNext}
+                    className="mt-[0.5%] text-white  font-[800] bg-[#C70039] hover:bg-red-800 rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-5"
+                  >
+                    Next step
+                  </button>
+                )}
+                {/* Submit button */}
+                {step === 3 && (
+                  <button
+                    type="submit"
+                    className="mt-[0.5%] text-white bg-[#C70039] hover:bg-red-800 font-[700] rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 disabled:bg-[#F1F2F6] disabled:text-[#646D89] ml-5"
+                  >
+                    Confirm
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
