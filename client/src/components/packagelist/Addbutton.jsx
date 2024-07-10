3dc0e038b4605775c65e1c270d2060645ab1b692
tanayod <tanayod741@gmail.com>
2024-07-10 20:25:46 +0700

    feat:add admin component V1

---
+++ client/src/components/packagelist/Addbutton.jsx
@@ -0,0 +1,8 @@
+function AddButton() {
+  return (
+    <button className="w-[160px] h-[48px] ml-[16px] rounded-[99px] bg-[#C70039] text-white">
+      + Add Package
+    </button>
+  );
+}
+export default AddButton;
