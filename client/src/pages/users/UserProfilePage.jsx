3bc761e002cff47555a588eccf4d688bd06b1300
kornkamon view <kornkamon.lert@gmail.com>
2024-07-13 23:59:18 +0700

    fix: edit UI user-profile-page version 2

--- client/src/pages/users/UserProfilePage.jsx
+++ client/src/pages/users/UserProfilePage.jsx
@@ -4,7 +4,6 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import plus from "../../assets/profilepicture/plus.png";
 import Footer from "../../components/homepage/Footer";
-import ProfilePictures from "../../components/registerpage/ProfilePictures";
 import NavbarAuthen from "../../components/navbar/NavbarAuthen";
 
 function UserProfilePage() {
@@ -108,8 +107,8 @@ function UserProfilePage() {
   return (
     <>
       <div className="lg:w-screen w-auto lg:h-screen bg-[#FCFCFE] flex flex-col gap-[80px]">
-        <div className="lg:w-screen w-auto mx-auto">
-          <nav className="w-auto lg:mb-[120px] mb-[50px] ">
+        <div className="lg:w-screen w-auto mx-auto  bg-[#FCFCFE]">
+          <nav className="lg:w-[562.67px] w-auto lg:mb-[120px] mb-[50px]">
             <NavbarAuthen />
           </nav>
 
@@ -120,11 +119,11 @@ function UserProfilePage() {
                   PROFILE
                 </p>
 
-                <h2 className="text-[#A62D82] text-[46px] leading-[57.5px] font-extrabold text-left lg:w-[517px] w-auto lg:h-[116px] h-auto flex-shrink-0">
+                <h2 className="text-[#A62D82] text-[46px] leading-[57.5px] font-extrabold text-left lg:w-[517px] w-auto lg:h-[116px] h-auto">
                   Let’s make profile <br /> to let others know you
                 </h2>
               </header>
-              <div className="lg:w-[414px] w-[338px] lg:h-[145px] h-[48px] flex flex-row items-end justify-center lg:mt-0 mt-[2330px] mx-auto">
+              <div className="lg:w-[414px] w-[338px] lg:h-[145px] h-[48px] flex flex-row items-end justify-center lg:mt-0 mt-[2450px] mx-auto">
                 <div className="flex gap-[16px]">
                   <button className="w-[162px] h-[48px] p-[12px, 24px, 12px, 24px] rounded-full p-[12px 24px] bg-[#FFE1EA] text-[#95002B] text-center font-bold text-base leading-6">
                     Preview Profile
@@ -135,11 +134,11 @@ function UserProfilePage() {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-screen lg:w-[930px] mt-[40px]">
+            <div className="flex flex-col w-full lg:pl-0 lg:pr-0 pl-[16px] pr-[16px] lg:w-[930px] lg:mt-[5px] mt-[40px]">
               <h1 className="basicInformation text-[#2A2E3F] font-[700] text-[24px]">
                 Basic Information
               </h1>
-              <div className="column1 flex mt-[24px] lg:flex-row flex-col">
+              <div className="column1 flex mt-[24px] lg:flex-row flex-col-reverse lg:gap-0 gap-[30px]">
                 <div className="flex flex-col lg:mr-[12px]">
                   <label
                     htmlFor="name"
@@ -175,10 +174,11 @@ function UserProfilePage() {
                     onChange={(date) => {
                       setSelectDate(date);
                     }}
+                    disabled
                   />
                 </div>
               </div>
-              <div className="column2 flex mt-[24px] lg:mt-[40px] lg:flex-row flex-col">
+              <div className="column2 flex mt-[24px] lg:mt-[40px] lg:flex-row flex-col-reverse lg:gap-0 gap-[30px]">
                 <div className="flex flex-col lg:mr-[12px]">
                   <label
                     htmlFor="location"
@@ -232,7 +232,7 @@ function UserProfilePage() {
                 </div>
               </div>
 
-              <div className="column2 flex mt-[24px] lg:mt-[40px] lg:flex-row flex-col">
+              <div className="column2 flex mt-[24px] lg:mt-[40px] lg:flex-row flex-col-reverse lg:gap-0 gap-[30px]">
                 <div className="flex flex-col lg:mr-[12px]">
                   <label
                     htmlFor="username"
@@ -271,11 +271,11 @@ function UserProfilePage() {
               </div>
             </div>
 
-            <div className="flex flex-col w-screen lg:w-[930px]">
+            <div className="flex flex-col w-full lg:pl-0 lg:pr-0 pl-[16px] pr-[16px] lg:w-[930px] lg:gap-0 gap-[-30px]">
               <h1 className="IdentitiesAndInterestes text-[#2A2E3F] font-[700] text-[24px] leading-[30px]">
                 Identities and Interests
               </h1>
-              <div className="column1 flex mt-[24px] lg:flex-row flex-col">
+              <div className="column1 flex mt-[24px] lg:flex-row flex-col-reverse lg:gap-0 gap-[30px]">
                 <div className="flex flex-col lg:mr-[12px] mb-[12px] lg:mb-0">
                   <label
                     htmlFor="SexualIdentities"
@@ -325,7 +325,7 @@ function UserProfilePage() {
                 </div>
               </div>
 
-              <div className="column2 flex mt-[40px] lg:flex-row flex-col">
+              <div className="column2 flex mt-[40px] lg:flex-row flex-col-reverse lg:gap-0 gap-[30px]">
                 <div className="flex flex-col lg:mr-[12px] mb-[12px] lg:mb-0">
                   <label
                     htmlFor="RacialPreferences"
@@ -362,7 +362,7 @@ function UserProfilePage() {
                   </select>
                 </div>
 
-                <div className="flex flex-col lg:ml-[12px] lg:mt-[0px] mt-[24px]">
+                <div className="flex flex-col lg:ml-[12px] lg:mt-[0px] mt-[24px] ">
                   <label
                     htmlFor="MeetingInterests"
                     className="font-[400] text-[16px] leading-[24px]"
@@ -426,17 +426,16 @@ function UserProfilePage() {
                   >
                     About me (Maximum 150 characters)
                   </label>
-                  <div className="flex flex-row flex-wrap border-[1px] border-[#D6D9E4] rounded-[8px] gap-[8px] pt-[12px] pr-[16px] pb-[12px] pl-[12px] w-full lg:w-[930px] mt-[4px]">
-                    <textarea
-                      className="textarea w-[931px] h-[120px] rounded-[8px] border border-1 pt-3 pl-3 pb-3 pr-3 resize-none"
-                      placeholder="I know nothing..but you"
-                    ></textarea>
-                  </div>
+
+                  <textarea
+                    className="textarea lg:w-[931px] w-auto h-[120px] rounded-[8px] border border-1 pt-3 pl-3 pb-3 pr-3 resize-none"
+                    placeholder="I know nothing..but you"
+                  ></textarea>
                 </div>
               </div>
             </div>
 
-            <div className="flex flex-col w-screen lg:w-[930px]">
+            <div className="flex flex-col w-full lg:pl-0 lg:pr-0 pl-[16px] pr-[16px] lg:w-[930px]">
               <div className="section-header">
                 <h2 className=" text-[#A62D82] text-[24px] font-[700] leading-[30px] lg:text-left">
                   Profile pictures
@@ -445,7 +444,7 @@ function UserProfilePage() {
                   Upload at least 2 photos
                 </p>
               </div>
-              <div className="picture-list lg:w-auto w-[375px] flex lg:flex-row flex-wrap lg:gap-[22px] gap-[22px] mt-[24px] mx-auto lg:mx-0">
+              <div className="picture-list lg:w-auto flex lg:flex-row flex-wrap lg:gap-[22px] gap-[22px] mt-[24px] mx-auto lg:mx-0 w-full">
                 {Object.keys(avatars).map((avatarKey) => (
                   <div
                     key={avatarKey}
@@ -489,7 +488,7 @@ function UserProfilePage() {
                 ))}
               </div>
             </div>
-            <div className="w-[128px] h-[32px] rounded-[16px] pt-[4px] pr-[8px] pb-[4px] pl-[8px] lg:mt-[-10px] mt-[40px]">
+            <div className="w-[128px] h-[32px] rounded-[16px] pt-[4px] pr-[8px] pb-[4px] pl-[8px] lg:mt-[-10px] mt-[60px]">
               <button className="w-[112px] h-[24px] font-semibold text-[16px] leading-[24px] text-[#646D89]">
                 Delete account
               </button>
