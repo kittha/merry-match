a02954521da3c166ffee3af635b1376644ff89ff
Namfon <varisa.vb@hotmail.com>
2024-07-15 11:00:34 +0700

    chore: remove unused folder and file

--- server/models/profile.model.mjs
+++ server/models/profile.model.mjs
@@ -1,49 +1,4 @@
 import connectionPool from "../configs/db.mjs";
-import cloudinaryUpload from "../utils/cloudinary.uploader.mjs";
-
-/**
- *
- * @param {number} userId
- * @param {object} files - The Object which contain key:value pair. Key is avatar. Value is Array of Objects. Each Object contain key:value pair of fieldname, originalname, encoding, mimetype, destination, filename, path, size.
- * @returns {boolean}
- */
-//ยังใช้อยู่มั้ย??
-export const uploadAvatar = async (userId, files) => {
-  try {
-    const fileUrls = await cloudinaryUpload(files);
-
-    if (!Array.isArray(fileUrls) || fileUrls.length === 0) {
-      throw new Error("No file URLs found from Cloudinary response");
-    }
-
-    for (let i = 0; i < fileUrls.length; i++) {
-      const pictureURIs = fileUrls[i];
-
-      const result = await connectionPool.query(
-        `
-
-        INSERT INTO profile_pictures (profile_id, cloudinary_id, url)
-        VALUES ($1, $2, $3)
-        RETURNING *
-        `,
-        [userId, pictureURIs.publicId, pictureURIs.url]
-      );
-      if (result.rowCount === 0) {
-        throw new Error(
-          `Failed to update profile picture for user_id ${userId}`
-        );
-      }
-      console.log(
-        `Inserted or updated profile picture with ID ${result.rows[0].profile_id}`
-      );
-    }
-
-    return true;
-  } catch (error) {
-    console.error("Error updating avatar:", error);
-    throw error;
-  }
-};
 
 export const createProfile = async (userId, data) => {
   const {
