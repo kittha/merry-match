2f5b75e7b93d0431ca9b223565c332ab6be02078
Namfon <varisa.vb@hotmail.com>
2024-07-17 11:07:07 +0700

    feat(client): add mobile UI of package page

--- client/src/pages/users/MerryPackagePage.jsx
+++ client/src/pages/users/MerryPackagePage.jsx
@@ -5,26 +5,25 @@ const MerryPackage = () => {
   const arr = [1, 2, 3];
   return (
     <div className="page-container h-screen w-screen flex flex-col items-center bg-[#FCFCFE]">
-      <div className="content-container lg:h-[663px] lg:w-[1119px] h-auto flex flex-col items-center lg:items-end gap-[80px] mt-[168px] mb-[160px] font-Nunito">
-        {/* <div className="lg:w-[931px] h-[145px] w-auto flex lg:flex-row flex-col lg:gap-[80px] gap-[20px]"></div> */}
-        <header className="lg:w-full h-[145px] flex flex-col lg:mt-0 mt-[60px] lg:pl-0 pl-[20px]">
-          <p className="text-[#7B4429] font-semibold text-sm h-[29px]">
+      <div className="content-container lg:h-[663px] lg:w-[1119px] h-auto flex flex-col items-center lg:gap-[80px] gap-[43px] mt-[52px] lg:mt-[168px] lg:mb-[160px] lg:p-0 px-4 py-10 font-Nunito">
+        <header className="lg:w-full lg:h-[145px] flex flex-col gap-2 mt-0 p-0">
+          <p className="text-[#7B4429] font-semibold text-sm lg:h-[29px]">
             MERRY MEMBERSHIP
           </p>
-          <h2 className="text-[#A62D82] text-[46px] leading-[57.5px] font-extrabold text-left w-auto lg:h-[116px] h-auto">
+          <h2 className="text-[#A62D82] text-3xl lg:text-[46px] lg:leading-[57.5px] font-bold lg:font-extrabold text-left w-auto lg:h-[116px] h-auto">
             Be part of Merry Membership
             <br />
             to make more Merry!
           </h2>
         </header>
-        <main className="card-container lg:w-full lg:h-[438px] flex lg:flex-row gap-6">
-          {arr.map((value) => {
+        <main className="card-container lg:w-full lg:h-[438px] flex flex-col lg:flex-row gap-6">
+          {arr.map(() => {
             return (
-              <div className="card w-[357px] h-full border border-[#D6D9E4] rounded-[32px] p-10 flex flex-col gap-6 bg-white">
+              <div className="card w-[357px] h-full border border-[#D6D9E4] rounded-[32px] p-4 lg:p-10 flex flex-col gap-4 lg:gap-6 bg-white">
                 <img
                   src="#"
                   alt="icon-package"
-                  className="icon w-[60px] h-[60px] bg-gray-500"
+                  className="icon w-[60px] h-[60px] rounded-2xl bg-[#F6F7FC]"
                 />
                 <div className="top w-full h-[78px] flex flex-col gap-2">
                   <h3 className="text-[#411032] text-[32px] leading-10 font-bold h-10">
