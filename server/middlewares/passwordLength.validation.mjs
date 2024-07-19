20d0dda2bf96ebd09d5c03d4635bb6aa7f0ee8b4
TiTY <tee25450@gmail.com>
2024-07-19 15:55:33 +0700

    fix: fix logic validation of password&username

--- server/middlewares/passwordLength.validation.mjs
+++ server/middlewares/passwordLength.validation.mjs
@@ -1,7 +1,7 @@
 export const validatePasswordLength = (req, res, next) => {
   const { password } = req.body;
 
-  if (!password || password.length <= 8) {
+  if (!password || password.length < 8) {
     return res
       .status(400)
       .json({ error: "Password must be longer than 8 characters." });
