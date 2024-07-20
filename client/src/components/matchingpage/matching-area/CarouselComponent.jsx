7aa86d21b1bdc667e5455e9304230448fdf01bba
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-20 11:14:05 +0700

    feat: create CarouselComponent, MerryLimitTodayComponent, SampleSwipeArrowComponent and implement in MatchingArea

--- client/src/components/matchingpage/matching-area/CarouselComponent.jsx
+++ client/src/components/matchingpage/matching-area/CarouselComponent.jsx
@@ -1,11 +1,43 @@
 import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const db = [
-  { name: "First Item", url: "/assets/matchingpage/matching-area/daeny.png" },
-  { name: "Second Item", url: "/assets/matchingpage/matching-area/daeny2.png" },
-  { name: "Third Item", url: "/assets/matchingpage/matching-area/daeny3.png" },
-];
+import { SampleNextArrow } from "./SampleSwipeArrow";
+import { SamplePrevArrow } from "./SampleSwipeArrow";
 
-const CarouselComponent = () => {};
+const CarouselComponent = ({ db }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "15%", // This shows 1/3 of the next and previous images
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  return (
+    <div className="relative">
+      <Slider {...settings} className="relative">
+        {db.map((user) => (
+          <div key={user.name} className="px-2 relative">
+            <img
+              src={user.url}
+              alt={user.name}
+              className="rounded-3xl w-full"
+            />
+            <p className="absolute bottom-10 left-10 right-0 text-left text-white font-bold">
+              <span>{user.name}</span>&nbsp;&nbsp;<span>{user.age}</span> <br />
+              <span>{user.location}</span>
+            </p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
 export default CarouselComponent;
---
+++ client/src/components/matchingpage/matching-area/MerryLimitToday.jsx
@@ -0,0 +1,11 @@
+const MerryLimitToday = () => {
+  return (
+    <div className="fixed bottom-4 right-4">
+      <p className="">
+        Merry limit today <span>PLACEHOLDER</span>
+      </p>
+    </div>
+  );
+};
+
+export default MerryLimitToday;
