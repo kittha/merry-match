6ba3fed21dd654d8ed61f3d3d9df7db2c8eda830
Namfon <varisa.vb@hotmail.com>
2024-07-24 21:15:23 +0700

    feat: add message controller structure

--- server/controllers/message.controller.mjs
+++ server/controllers/message.controller.mjs
@@ -1,7 +1,28 @@
 import { createMessage, getMessages } from "../models/message.model.mjs";
 
-export const sendMessage = async (req, res) => {};
-
-export const getMessage = async (req, res) => {
+export const sendMessage = async (req, res) => {
   const { matchId } = req.params;
+  try {
+    // const result = await createMessage(data);
+    return res.status(200).json({});
+  } catch (error) {
+    console.error("Error in message controller:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getChatHistory = async (req, res) => {
+  const { matchId } = req.params;
+  try {
+    const result = await getMessages(matchId);
+    console.log(result);
+    return res.status(200).json({});
+  } catch (error) {
+    console.error("Error in message controller:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
