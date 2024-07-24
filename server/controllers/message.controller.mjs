473ffbb6b0be2b8f5ee4ec10005d04356afe574a
Namfon <varisa.vb@hotmail.com>
2024-07-24 17:09:26 +0700

    feat: add chat message model

---
+++ server/controllers/message.controller.mjs
@@ -0,0 +1,7 @@
+import { createMessage, getMessages } from "../models/message.model.mjs";
+
+export const sendMessage = async (req, res) => {};
+
+export const getMessage = async (req, res) => {
+  const { matchId } = req.params;
+};
