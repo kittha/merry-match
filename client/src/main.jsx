32d9ef2f4e79c5d1b5ebab3e2b0454208e8e7e5f
TiTY <tee25450@gmail.com>
2024-07-25 10:34:07 +0700

    fix: resolve merge bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import jwtInterceptor from "./utils/jwtInterceptor.js";
 import { AuthProvider } from "./contexts/authentication.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { FormProvider } from "./contexts/FormProvider.jsx";
-import DevToolbar from "./components/debugger/DevToolbar.jsx";
+
 
 import { PackageProvider } from "./contexts/PackageProvider.jsx";
 
@@ -25,7 +25,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
         <PackageProvider>
           <FormProvider>
-            {/* <DevToolbar /> */}
             <App />
           </FormProvider>
         </PackageProvider>
