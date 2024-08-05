a414745cb798d688c8e80cc69980cde594ad7952
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-08-05 12:50:59 +0700

    docs: add TODO and TOFIX comments in code

--- server/middlewares/multer.middleware.mjs
+++ server/middlewares/multer.middleware.mjs
@@ -1,5 +1,11 @@
 import multer from "multer";
 
+// FIXME high priority : must create fileUploadValidation module
+// to prevent malicious file upload
+// such as : malicious file type, malicious file size, etc
+// thing to do : limit file size, type;
+// additional: verify file type; sanitize filenames and path; use VirusTotal API to scan file
+//
 const multerUpload = multer({ dest: "uploads/" });
 const avatarUpload = multerUpload.fields([{ name: "avatar", maxCount: 5 }]);
 
